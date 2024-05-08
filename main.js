e3acceb8111dc64d9f83276db1134b5ca2ecd889
DIOGOPINDUCA <107262227+DIOGOPINDUCA@users.noreply.github.com>
2024-05-08 11:20:00 -0300

    08/05/2024

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ for(let i=0;i <botoes.length;i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-03-28T00:00:00");
-const tempoObjetivo2 = new Date("2024-06-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2025-01-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-22T00:00:00");
+const tempoObjetivo3 = new Date("2024-08-15T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-10T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
